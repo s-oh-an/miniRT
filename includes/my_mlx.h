efb7aa8c07935de54b2fde309e290af91c82f501
anso33 <hyeon131812@gmail.com>
2022-08-12 17:17:50 +0900

    [ADD] new header

---
+++ includes/my_mlx.h
@@ -0,0 +1,33 @@
+
+
+#ifndef MY_MLX_H
+# define MY_MLX_H
+
+# include "../lib/mlx/mlx.h" //
+# include "../includes/discriminant.h"
+
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*win;
+	t_data	data;
+}	t_mlx;
+
+void	init_mlx(t_mlx *m, t_window w);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int		press_key(int key_code, t_mlx *m);
+
+
+
+
+#endif
---
+++ includes/ray.h
@@ -0,0 +1,22 @@
+
+
+
+
+#ifndef RAY_H
+# define RAY_H
+
+# include "scene.h"
+# include "vector.h"
+# include "discriminant.h"
+# include "my_mlx.h"
+
+typedef struct s_ray
+{
+	// 정규화된 ray의 방향벡터
+	t_vec	vec;
+}	t_ray;
+
+t_ray	init_ray(float x, float y, float z);
+void	shoot_ray(t_mlx *m, t_camera cam, t_window win);
+
+#endif
