0e0a9b0363f83e08b1b7740ccb919d163d918f04
anso33 <hyeon131812@gmail.com>
2022-08-18 18:17:39 +0900

    [feat] add hit_record (to recognize near point) func

--- src/discriminant/hit.c
+++ src/discriminant/hit.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 
+#include <stdio.h>
 
 //(1을 리턴 -> 판별식함수가 1을 리턴하는 곳이 이 함수를 사용 )
 int	update_hit(t_ray *ray, t_ray new)
@@ -11,6 +12,8 @@ int	update_hit(t_ray *ray, t_ray new)
 	if (ray->hit.t > new.hit.t)	// - + 일떄는 + 값이 교점 
 	{
 		*ray = new;
+		// printf("nvertor x : %f y : %f z : %f\n", ray->hit.hit_normal.x, ray->hit.hit_normal.y, ray->hit.hit_normal.z);
+
 	}
 	return (1);
 }
@@ -22,12 +25,10 @@ t_ray	make_hit_sphere(t_sphere *sphere, float *t, t_ray *ray)
 	t_ray	new;
 
 	new.vec = ray->vec;
-
 	if (t[0] * t[1] < 0)
 		new.hit.t = t[1];
 	else
 		new.hit.t = t[0];
-
 	new.hit.hit_point = vmulti_f(new.vec, new.hit.t);
 	new.hit.hit_color = sphere->color;
 	new.hit.hit_normal = vunit(vminus(new.hit.hit_point, sphere->coordinate));
@@ -35,12 +36,7 @@ t_ray	make_hit_sphere(t_sphere *sphere, float *t, t_ray *ray)
 		new.hit.in_object = 0;
 	else 
 		new.hit.in_object = 1;
-	////// 내적이 0일떄는??/??
-	// 1. 접할 때 -> 밖, 0
-
 	new.hit.min = 0;
-
-	// ㅇ것도 update_hit에서 정할거
 	new.hit.ray_hit = 1;
 	return (new);
 }
@@ -50,39 +46,30 @@ t_ray	make_hit_plane(t_plane *plane, float t, t_ray *ray)
 	t_ray	new;
 
 	new.vec = ray->vec;
-
 	new.hit.t = t;
-
 	new.hit.hit_point = vmulti_f(new.vec, new.hit.t);
-	
 	new.hit.hit_color = plane->color;
-
 	new.hit.hit_normal = vunit(plane->n_vector);
-
 	if (vdot(ray->vec, new.hit.hit_normal) <= 0)
 		new.hit.in_object = 0;
 	else 
 		new.hit.in_object = 1;
-	////// 내적이 0일떄는??/??
-	// 1. 접할 때 -> 밖, 0
-
 	new.hit.min = 0;
-
-	// ㅇ것도 update_hit에서 정할거
-	//new.hit.ray_hit = 0;
+	new.hit.ray_hit = 1;
 	return (new);
 }
 
-t_ray	make_hit_cylinder(t_cylinder *cylinder, float *t, t_ray *ray)
+t_ray	make_hit_cylinder(t_cylinder *cylinder, float t, t_ray *ray)
 {
 	t_ray	new;
 	t_vec	r_n_vector;
 
 	new.vec = ray->vec;
-	if (t[0] * t[1] < 0)
-		new.hit.t = t[1];
-	else
-		new.hit.t = t[0];
+	// if (t[0] * t[1] < 0)
+	// 	new.hit.t = t[1];
+	// else
+	// 	new.hit.t = t[0];
+	new.hit.t = t;
 	new.hit.hit_point = vmulti_f(new.vec, new.hit.t);	
 	new.hit.hit_color = cylinder->color;
 	r_n_vector = vmulti_f(cylinder->n_vector, -1.0);
@@ -99,11 +86,7 @@ t_ray	make_hit_cylinder(t_cylinder *cylinder, float *t, t_ray *ray)
 		new.hit.in_object = 0;
 	else 
 		new.hit.in_object = 1;
-	////// 내적이 0일떄는??/??
-	// 1. 접할 때 -> 밖, 0
 	new.hit.min = 0;
-	// ㅇ것도 update_hit에서 정할거
 	new.hit.ray_hit = 1;
-
 	return (new);
 }
