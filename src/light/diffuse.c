71e5a010c0c069f3d197d8489cc8fcbb0b37e620
anso33 <hyeon131812@gmail.com>
2022-08-19 14:38:47 +0900

    [fix] fix unit

--- src/light/diffuse.c
+++ src/light/diffuse.c
@@ -9,7 +9,9 @@ t_color	get_pixel_diffuse_color(t_scene *scene, t_ray *ray)
 	t_vec	l;
 
 	diffuse = vec3(1, 1, 1);
-	l = vunit(vminus(ray->hit.hit_point, scene->light.coordinate));
+	l = vunit(vminus(vunit(ray->hit.hit_point), vunit(scene->light.coordinate)));
+	// if (ray->hit.in_object) // 빛이 물체 안에 있는지 밖에 있는지 판별 필요 
+	// 	ray->hit.hit_normal = vmulti_f(ray->hit.hit_normal, -1);
 	diffuse = vmulti_f(diffuse, vdot(ray->hit.hit_normal, l) * -scene->light.ratio);
 	return (vmax(vec3(0, 0, 0), diffuse));
 }
